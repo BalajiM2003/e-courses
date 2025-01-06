1aa05e77113cf179fe3d1837e364c1ba357bff1d
Balaji M <balajiking435@gmail.com>
2025-01-06 17:54:56 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import { Header } from './Components/Header'
-import { Routers } from './Routers'
 import ScrollToTop from './ScrollToTop';
 import AuthContext from './AuthContext'
 import { Footer } from './Components/Footer';
 import { CartProvider } from './CartContext';
-
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './Pages/Login';
+import { SignUp } from './Pages/SignUp';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import { Contact } from './Pages/Contact';
+import { Event } from './Pages/Event';
+import TutorDetails from './Pages/TutorDetails';
+import { CompleteDetails } from './Pages/CompleteDetails';
+import SearchResults from './Pages/SearchResult';
+import { Cart } from './Cart';
+import { Profile } from './Pages/Profile';
+import { Details } from './Pages/Details';
+import PaymentPage from './Pages/PaymentPage';
 function App() {
 
 
@@ -19,8 +31,23 @@ function App() {
         <AuthContext.AuthProvider>
           <CartProvider>
             <Header />
-            <Routers />
-            <Footer />
+
+            <Routes>
+
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/event' element={<Event />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='/tutor/:name' element={<CompleteDetails />} />
+              <Route path="/course/:courseTitle" element={<TutorDetails />} />
+              <Route path='/search-results' element={<SearchResults />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/products' element={<Details />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/payment' element={<PaymentPage />} />
+            </Routes>            <Footer />
           </CartProvider>
         </AuthContext.AuthProvider>
       </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 
   plugins: [react()],
-  base: "/e-courses/",
+  base: "/",
   build: {
     outDir: 'build', // Output directory for GitHub Pages
   },
